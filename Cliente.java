546ef964ecdd20be856b7bf3a8392252f9e3242e
embozza <153663385+embozza@users.noreply.github.com>
2024-08-02 11:44:17 -0300

    Create Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,25 @@
+public class Cliente {
+    private String nome;
+    private Carro carro;
+
+    public Cliente(String nome, Carro carro) {
+        this.nome = nome;
+        this.carro = carro;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Carro getCarro() {
+        return carro;
+    }
+
+    public void setCarro(Carro carro) {
+        this.carro = carro;
+    }
+}
